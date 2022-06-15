d798d73d50ad60713f3f802d24862297b8fb6c29
Berd_ur18@itstepru.onmicrosoft.com <Berd_ur18@itstepru.onmicrosoft.com>
2022-06-15 15:19:24 +0500

    complite 1.0

--- ElectroBicycle13.06.2022/ElectroBicycle.cpp
+++ ElectroBicycle13.06.2022/ElectroBicycle.cpp
@@ -3,22 +3,25 @@ using namespace std;
 
 class Transport {
 public:
+	Transport(int numberOfSeats, int maxVelocity, int weight)
+		:numberOfSeats(numberOfSeats), maxVelocity(maxVelocity), weight(weight){}
+
 	void setNumberOfSeats(int numberOfSeats) {
 		numberOfSeats = numberOfSeats;
 	}
-	int getNumberOfSeats() {
+	int getNumberOfSeats() const{
 		return numberOfSeats;
 	}
 	void setMaxVelocity(int maxVelocity) {
 		maxVelocity = maxVelocity;
 	}
-	int getMaxVelocity() {
+	int getMaxVelocity() const{
 		return maxVelocity;
 	}
 	void setWeight(int weight) {
 		weight = weight;
 	}
-	int getWeight() {
+	int getWeight() const{
 		return weight;
 	}
 private:
@@ -29,39 +32,81 @@ private:
 
 class Musscular : virtual public Transport {
 public:
+	Musscular(int countOfWorkers):Transport(0, 0, 0) {}
 
+	void setCountOfWorkers(int ountOfWorkers) {
+		ountOfWorkers = ountOfWorkers;
+	}
+	int getCountOfWorkers() const{
+		return countOfWorkers;
+	}
 private:
 	int countOfWorkers;
 };
 
 class Motor : virtual public Transport {
 public:
+	Motor(int horsePower):Transport(0, 0, 0){}
 
+	void setHorsePower(int horsePower) {
+		horsePower = horsePower;
+	}
+	int getHorsePower() const{
+		return horsePower;
+	}
 private:
-	int power;
+	int horsePower;
 };
 
 class Bicycle : public Musscular {
+public:
+	Bicycle(int numberOfPedals): Musscular(1), Transport(1, 15, 500){}
 
+	void setNumberOfPedals(int numberOfPedals) {
+		numberOfPedals = numberOfPedals;
+	}
+	int getNumberOfPedals() {
+		return numberOfPedals;
+	}
+private:
+	int numberOfPedals;
 };
 
 class Scate : public Musscular { //Самокаты
 
 };
 
-class DVS : public Motor {
+class ICE : public Motor {
 
 };
 
 class electricCar : public Motor {
+public:
+	electricCar(bool autopilot) : Motor(4), Transport(4, 150, 1000){}
 
+	void setAutopilot(bool autopilot) {
+		autopilot = autopilot;
+	}
+	bool getAutopilot() {
+		/*autopilot ? cout << "Autopilot yes" << endl
+			: cout << "No autopilot" << endl;*/
+		return autopilot;
+	}
+private:
+	bool autopilot;
 };
 
-class electricBike {
-
+class electricBike : public Bicycle, public electricCar{
+public:
+	electricBike() :Bicycle(0), electricCar(true), Transport(1, 15, 600) {}
 };
 
 int main() {
+
+	electricBike eb;
+
+	eb.getAutopilot();
+	eb.getMaxVelocity();
 
 	return 0;
 }
